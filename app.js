530f6e3de90f38b5372443d7deb66f515a4cc758
Miguel Lahoz <miguel.lahoz@gmail.com>
2015-09-20 20:09:57 +0200

    Serving other static resources

--- app.js
+++ app.js
@@ -2,22 +2,30 @@ var http = require("http"),
     // utilities for working with file paths
     path = require("path"),
     // utilities for accessing the file system
-    fs = require("fs");
+    fs = require("fs"),
+    extensions = {
+        ".html": "text/html",
+        ".css": "text/css",
+        ".js": "application/javascript",
+        ".png": "image/png",
+        ".gif": "image/gif",
+        ".jpg": "image/jpeg"
+    };
 
 http.createServer(function(req, res) {
 
     // look for a filename in the URL, default to index.html
     var filename = path.basename(req.url) || "index.html",
         ext = path.extname(filename),
+        dir = path.dirname(req.url).substring(1),
         // __dirname is a built-in variable containing the path where the code is running
         localPath = __dirname + "/public/";
 
-    if (ext === ".html") {
-        localPath += filename;
-        // verify that this file actually exists and load it, or else return a 404
+    if (extensions[ext]) {
+        localPath += (dir ? dir + "/" : "") + filename;
         fs.exists(localPath, function(exists) {
             if (exists) {
-                getFile(localPath, res);
+                getFile(localPath, extensions[ext], res);
             } else {
                 res.writeHead(404);
                 res.end();
@@ -27,11 +35,13 @@ http.createServer(function(req, res) {
 
 }).listen(8000);
 
-function getFile(localPath, res) {
-    // read the file in and return it, or return a 500 if it can't be read
+function getFile(localPath, mimeType, res) {
     fs.readFile(localPath, function(err, contents) {
         if (!err) {
-            // use defaults instead of res.writeHead()
+            res.writeHead(200, {
+                "Content-Type": mimeType,
+                "Content-Length": contents.length
+            });
             res.end(contents);
         } else {
             res.writeHead(500);
