5f1357312035104e2991f426632d313289207948
Miguel Lahoz <miguel.lahoz@gmail.com>
2015-09-20 18:03:09 +0200

    Serving static html files

--- app.js
+++ app.js
@@ -1,16 +1,41 @@
-var http = require("http");
+var http = require("http"),
+    // utilities for working with file paths
+    path = require("path"),
+    // utilities for accessing the file system
+    fs = require("fs");
 
 http.createServer(function(req, res) {
-    var html = "<!doctype html>" +
-        "<html><head><title>Hello world</title></head>" +
-        "<body><h1>Hello, world!</h1></body></html>";
 
-    res.writeHead(200, {
-        // set the type of content we're returning
-        "Content-Type": "text/html",    // set the length of our content
-        "Content-Length": html.length
+    // look for a filename in the URL, default to index.html
+    var filename = path.basename(req.url) || "index.html",
+        ext = path.extname(filename),
+        // __dirname is a built-in variable containing the path where the code is running
+        localPath = __dirname + "/public/";
+
+    if (ext === ".html") {
+        localPath += filename;
+        // verify that this file actually exists and load it, or else return a 404
+        fs.exists(localPath, function(exists) {
+            if (exists) {
+                getFile(localPath, res);
+            } else {
+                res.writeHead(404);
+                res.end();
+            }
+        });
+    }
+
+}).listen(8000);
+
+function getFile(localPath, res) {
+    // read the file in and return it, or return a 500 if it can't be read
+    fs.readFile(localPath, function(err, contents) {
+        if (!err) {
+            // use defaults instead of res.writeHead()
+            res.end(contents);
+        } else {
+            res.writeHead(500);
+            res.end();
+        }
     });
-
-    // end the response, sending it and returning our HTML
-    res.end(html);
-}).listen(8000, "127.0.0.1");
+}
