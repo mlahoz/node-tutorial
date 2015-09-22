ce15fdedda3ab0cc731f47e0545477f7a42c4f8a
Miguel Lahoz <miguel.lahoz@gmail.com>
2015-09-22 13:29:45 +0200

    Dynamic behavior with connect-route (and serve-static)

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
-var http = require("http"),
-    querystring = require("querystring");
+var connect = require("connect"),
+    connectRoute = require("connect-route"),
+    serveStatic = require("serve-static"),
+    app = connect();
 
-http.createServer(function(req, res) {
-    // parse everything after the "?" into key/value pairs
-    var qs = querystring.parse(req.url.split("?")[1]),
-        // property names are the same as in the querystring
-        userName = qs.firstName + " " + qs.lastName,
-        html = "<!doctype html>" +
-            "<html><head><title>Hello " + userName + "</title></head>" +
-            "<body><h1>Hello, " + userName + "!</h1></body></html>";
+app.use(connectRoute(function(router) {
+    router.get("/sayHello/:firstName/:lastName", function(req, res) {
+	var userName = req.params.firstName + " " + req.params.lastName,
+            html = "<!doctype html>" +
+		"<html><head><title>Hello " + userName + "</title></head>" +
+		"<body><h1>Hello, " + userName + "!</h1></body></html>";
 
-    res.end(html);
-}).listen(8000);
+	res.end(html);
+    });
+}));
+
+app.use(serveStatic("./public"));
+
+app.listen(8000);
