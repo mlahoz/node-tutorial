fad56fe712a72a5455f625be595abb37a231d143
Miguel Lahoz <miguel.lahoz@gmail.com>
2015-10-10 22:27:04 +0200

    Dynamic behavior with POST

--- app.js
+++ app.js
@@ -1,19 +1,41 @@
-var connect = require("connect"),
-    connectRoute = require("connect-route"),
-    serveStatic = require("serve-static"),
-    app = connect();
+var http = require("http"),
+    fs = require("fs"),
+    querystring = require("querystring");
 
-app.use(connectRoute(function(router) {
-    router.get("/sayHello/:firstName/:lastName", function(req, res) {
-	var userName = req.params.firstName + " " + req.params.lastName,
+http.createServer(function(req, res) { var data = "";
+    // serve static form
+    if (req.method == "GET") {
+        getFile(__dirname + "/public/simpleForm.html", "text/html", res); }
+
+    // handle form post
+    if (req.method == "POST") {
+        req.on("data", function(chunk) {
+            // append received data
+            data += chunk;
+        });
+        req.on("end", function() {
+            // get key/value pairs from received data
+            var params = querystring.parse(data),
+            userName = params.firstName + " " + params.lastName,
             html = "<!doctype html>" +
-		"<html><head><title>Hello " + userName + "</title></head>" +
-		"<body><h1>Hello, " + userName + "!</h1></body></html>";
+                "<html><head><title>Hello " + userName + "</title></head>" +
+                "<body><h1>Hello, " + userName + "!</h1></body></html>";
+            res.end(html);
+        });
+    }
+}).listen(8000);
 
-	res.end(html);
+function getFile(localPath, mimeType, res) {
+    fs.readFile(localPath, function(err, contents) {
+        if (!err) {
+            res.writeHead(200, {
+                "Content-Type": mimeType,
+                "Content-Length": contents.length
+            });
+            res.end(contents);
+        } else {
+            res.writeHead(500);
+            res.end();
+        }
     });
-}));
-
-app.use(serveStatic("./public"));
-
-app.listen(8000);
+}
