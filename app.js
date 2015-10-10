6c08a0e9ad9c9625eec00e254ccf4f37bfbef86a
Miguel Lahoz <miguel.lahoz@gmail.com>
2015-10-10 23:28:28 +0200

    POST with body-parser

--- app.js
+++ app.js
@@ -1,41 +1,15 @@
-var http = require("http"),
-    fs = require("fs"),
-    querystring = require("querystring");
+var connect = require("connect"),
+    bodyParser = require("body-parser"),
+    serveStatic = require("serve-static"),
+    app = connect();
 
-http.createServer(function(req, res) { var data = "";
-    // serve static form
-    if (req.method == "GET") {
-        getFile(__dirname + "/public/simpleForm.html", "text/html", res); }
-
-    // handle form post
-    if (req.method == "POST") {
-        req.on("data", function(chunk) {
-            // append received data
-            data += chunk;
-        });
-        req.on("end", function() {
-            // get key/value pairs from received data
-            var params = querystring.parse(data),
-            userName = params.firstName + " " + params.lastName,
-            html = "<!doctype html>" +
-                "<html><head><title>Hello " + userName + "</title></head>" +
-                "<body><h1>Hello, " + userName + "!</h1></body></html>";
-            res.end(html);
-        });
-    }
-}).listen(8000);
-
-function getFile(localPath, mimeType, res) {
-    fs.readFile(localPath, function(err, contents) {
-        if (!err) {
-            res.writeHead(200, {
-                "Content-Type": mimeType,
-                "Content-Length": contents.length
-            });
-            res.end(contents);
-        } else {
-            res.writeHead(500);
-            res.end();
-        }
-    });
-}
+app.use(serveStatic("./public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(function (req, res) {
+    var userName = req.body.firstName + " " + req.body.lastName,
+    html = "<!doctype html>" +
+        "<html><head><title>Hello " + userName + "</title></head>" +
+        "<body><h1>Hello, " + userName + "!</h1></body></html>";
+    res.end(html);
+})
+app.listen(8000);
