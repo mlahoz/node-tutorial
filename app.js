016b9481530ce8198d80aa77df1da6585bafc9d7
Miguel Lahoz <miguel.lahoz@gmail.com>
2015-10-11 17:46:50 +0200

    Partial templates (with requirejs/text)

--- app.js
+++ app.js
@@ -1,33 +1,49 @@
 var connect = require("connect"),
-    bodyParser = require("body-parser"),
+    connectRoute = require("connect-route"),
     serveStatic = require("serve-static"),
-    fs = require("fs"),
     mustache = require("mustache"),
+    requirejs = require("requirejs"),
+    parentTmpl,
     app = connect();
 
+// configure requirejs to fall back to Node's require if a module is not found
+requirejs.config({ nodeRequire: require });
+
 app.use(serveStatic("./public"));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(function (req, res) {
-    var userName = {
-            firstName: req.body.firstName,
-            lastName: req.body.lastName
-        },
-        // create and open the stream
-        tmplFile = fs.createReadStream(
-            __dirname + "/public/edit.html",
-            {encoding: "utf8"}
-        ),
-        template = "",
-        html;
+app.use(connectRoute(function(router) {
 
-    tmplFile.on("data", function(data) {
-        template += data;
+    router.get("/show/:tmpl/:firstName/:lastName", function(req, res) {
+        var userName = {
+                firstName: req.params.firstName,
+                lastName: req.params.lastName
+            };
+
+        // once the parent template is loaded, render the page
+        requirejs(["text!public/parent.html"], function(_parentTmpl) {
+            parentTmpl = _parentTmpl;
+            render(res, req.params.tmpl + ".html", userName);
+        });
     });
 
-    tmplFile.on("end", function() {
-        // render the template with the userName object as data
-        html = mustache.to_html(template, userName);
-        res.end(html);
-    });
-});
+}));
+
 app.listen(8000);
+
+function render(res, filename, data, style, script, callback) {
+    // load the template and return control to another function or send the response
+    requirejs(["text!public/" + filename], function(tmpl) {
+        if (callback) {
+            callback(res, tmpl, data, style, script);
+        } else {
+            // render parent template with page template as a child
+            var html = mustache.to_html(
+                parentTmpl,
+                data,
+                {content: tmpl, stylesheets: style || "", scripts: script || ""}
+            );
+            res.end(html);
+        }
+    });
+}
+
+// test: http://127.0.0.1:8000/show/hello/Mister/Magoo
